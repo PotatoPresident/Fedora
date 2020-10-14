c926b2b0e9b73fac175f938be607231f85355466
PotatoPresident <potatoboy9999@outlook.com>
2020-10-14 13:44:25 -0700

    Fixed stuff

--- src/main/java/us/potatoboy/fedora/component/PlayerHatComponent.java
+++ src/main/java/us/potatoboy/fedora/component/PlayerHatComponent.java
@@ -2,7 +2,6 @@ package us.potatoboy.fedora.component;
 
 import dev.onyxstudios.cca.api.v3.component.AutoSyncedComponent;
 import dev.onyxstudios.cca.api.v3.component.ComponentV3;
-import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundTag;
 import us.potatoboy.fedora.Fedora;
@@ -10,8 +9,8 @@ import us.potatoboy.fedora.Hat;
 import us.potatoboy.fedora.HatManager;
 
 public class PlayerHatComponent implements ComponentV3, AutoSyncedComponent {
-    private Hat currentHat = null;
-    private PlayerEntity playerEntity;
+    private Hat currentHat;
+    private final PlayerEntity playerEntity;
 
     public PlayerHatComponent (PlayerEntity playerEntity) {
         this.playerEntity = playerEntity;
@@ -31,7 +30,7 @@ public class PlayerHatComponent implements ComponentV3, AutoSyncedComponent {
         if (compoundTag.contains("currenthat")) {
             String hatName = compoundTag.getString("currenthat");
 
-            if(!HatManager.getHats().containsKey(currentHat)) return;
+            if(!HatManager.getHats().containsKey(currentHat.name)) return;
             setCurrentHat(new Hat(hatName));
         } else {
             setCurrentHat(null);
