3a9eac358fa5a0a104a49b01b17e6f1a53c9f860
PotatoPresident <potatoboy9999@outlook.com>
2020-10-28 10:09:15 -0700

    Download hats from github

--- src/main/java/us/potatoboy/fedora/HatLoader.java
+++ src/main/java/us/potatoboy/fedora/HatLoader.java
@@ -8,11 +8,13 @@ import net.devtech.arrp.json.blockstate.JState;
 import net.devtech.arrp.json.lang.JLang;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
@@ -37,11 +39,10 @@ public class HatLoader implements RRPPreGenEntrypoint {
         modelsFolder = new File(hatsFolder, "models");
         textFolder = new File(hatsFolder, "textures");
 
-        if (!modelsFolder.isDirectory()) {
-            modelsFolder.mkdirs();
-        }
-        if (!textFolder.isDirectory()) {
-            textFolder.mkdirs();
+
+        if (!hatsFolder.isDirectory()) {
+            LOGGER.info("Not hats installed, downloading hats from server");
+            downloadHatsFromServer();
         }
 
         File[] modelFiles = this.modelsFolder.listFiles(POSSIBLE_HAT);
@@ -63,13 +64,18 @@ public class HatLoader implements RRPPreGenEntrypoint {
                         JsonElement jsonElement = parser.parse(new FileReader(file));
                         jsonObject = jsonElement.getAsJsonObject();
 
-                        creator = jsonObject.get("credit").getAsString();
                     } catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
-
+                        LOGGER.severe("Failed to parse json for hat: " + file.getPath());
                     }
 
                     RESOURCE_PACK.addAsset(new Identifier("fedora", "models/block/" + hatName + ".json"), jsonObject.toString().getBytes());
                     RESOURCE_PACK.addBlockState(JState.state(JState.variant(JState.model("fedora:block/" + hatName))), new Identifier("fedora:" + hatName));
+
+                    try {
+                        creator = jsonObject.get("credit").getAsString();
+                    } catch (Exception e) {
+                        LOGGER.severe("Failed to read creator for hat: " + file.getPath());
+                    }
 
                     try {
                         jsonObject.get("lang").getAsJsonObject().entrySet().forEach(stringJsonElementEntry -> {
@@ -113,5 +119,38 @@ public class HatLoader implements RRPPreGenEntrypoint {
         }
 
         RRPCallback.EVENT.register(a -> a.add(RESOURCE_PACK));
+    }
+
+    private static JsonObject readJsonUrl(String urlString) throws Exception {
+        URL url = new URL(urlString);
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+
+        JsonParser parser = new JsonParser();
+        JsonElement jsonElement = parser.parse(reader);
+        return jsonElement.getAsJsonObject();
+    }
+
+    private void downloadHatsFromServer() {
+        try {
+            JsonObject jsonObject = readJsonUrl("https://raw.githubusercontent.com/PotatoPresident/Fedora/master/hats/hats.json");
+
+            JsonArray models = jsonObject.get("models").getAsJsonArray();
+            JsonArray textures = jsonObject.get("textures").getAsJsonArray();
+
+            modelsFolder.mkdir();
+            for (JsonElement model : models) {
+                File modelFile = new File(modelsFolder, model.getAsString() + ".json");
+                FileUtils.copyURLToFile(new URL("https://raw.githubusercontent.com/PotatoPresident/Fedora/master/hats/models/" + model.getAsString() + ".json"), modelFile, 10000, 10000);
+            }
+
+            textFolder.mkdir();
+            for (JsonElement texture : textures) {
+                File textureFile = new File(textFolder, texture.getAsString() + ".png");
+                FileUtils.copyURLToFile(new URL("https://raw.githubusercontent.com/PotatoPresident/Fedora/master/hats/textures/" + texture.getAsString() + ".png"), textureFile, 10000, 10000);
+            }
+        } catch (Exception e) {
+            LOGGER.info("Failed to download hats from server");
+            e.printStackTrace();
+        }
     }
 }
